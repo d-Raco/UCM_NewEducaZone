8b8a8827da6eb2de2ff6f4b8a221b58d2d1805d3
Rymond3 <ramoncos@ucm.es>
2020-05-11 02:30:48 +0200

    Update index.php

--- index.php
+++ index.php
@@ -7,82 +7,107 @@ require_once __DIR__ . '/include/config.php';
   <head>
     <meta charset="utf-8">
     <title>Index</title>
-    <!-- <link rel="stylesheet" type="text/css" href="css/estilo.css">
-    <link rel="stylesheet" type="text/css" href="css/index.css"> -->
-    <link rel="stylesheet" type="text/css" href="css/est.css">
-    <link rel="stylesheet" type="text/css" href="css/ind.css">
+    <link rel="stylesheet" type="text/css" href="css/estilo.css">
+    <link rel="stylesheet" href="css/index.css">
   </head>
 
   <body>
-
   <?php
      include("include/comun/cabecera.php");
   ?>
 
+<div class="containerr">
+  <img src="img/niño_cooreando.jpg" alt="Avatar" class="image" style=width:100%>
+  <div class="centered"> <font size="+8"> Educazone</font> </div>
+</div>
 
-
-
-
-    <br><br>
-
-<font size="+4"><center> Educazone permite la comunicación entre cualquier institución dedicada a la educación y
-    los padres de los alumnos que se encuentran en la misma mediante distintas
-    funcionalidades como la agenda personal del alumno, mensajería instantánea, la ficha
-    personal de cada alumno y el aula virtual donde se compartirán distintos contenidos de las
-    asignaturas. </center></font>
-
-<!--
-    <div class="container">
-      <img class="mainImage" src="img/prueba.jpg" alt="main Image">
-      <div class="center">Conecta con tu hijo</div>
-    </div>
--->
-
-          <!--  <img src="img/Chavala_haciendo_quimica.jpg" class="slider-image" />
-            <img src="img/cuaderno_y_lapiz.jpg" class="slider-image" />
-            <img src="img/Niños_haciendo_deberes.jpg" class="slider-image" />  -->
-
-            <div class="galeria" style="--w: 260px; --h: 300px;">
-    <input type="radio" name="navegacion" id="_1" checked>
-    <input type="radio" name="navegacion" id="_2">
-    <input type="radio" name="navegacion" id="_3">
-    <img src="img/Niños_haciendo_deberes.jpg" width="1846" height="970" alt="Galeria CSS 1" />
-    <div class="center" style="text-align:center" >Proporcionale a tu hijo la educación que se merece</div>
-    <img src="img/Chavala_haciendo_quimica.jpg" width="1846" height="970" alt="Galeria CSS 2"  />
-    <img src="img/niño_cooreando.jpg" width="1846" height="970" alt="Galeria CSS 3" />
-  </div>
-
-
-<font size="+4"><center>ACTIVIDADES: </center></font>
-
-    <div class="cuadrados">
-      <div class="cuadrado">
-        <h1>Acercamiento al profesor</h1>
-        <p>Ofrecemos un servicio de mensajería que te permitirá contactar con los profesores de forma sencilla y amigable para concertar citas o resolver dudas.</p>
-      </div>
-
-      <div class="cuadrado">
-        <h1>Agenda electrónica</h1>
-        <p>Revisa la agenda de tu hijo: deberes, exámenes, vacaciones, tutorías, etc.</p>
-      </div>
-
-      <div class="cuadrado">
-        <h1>Revisa incidencias</h1>
-        <p>Permitimos que los profesores pongan comentarios para que conozcas el rendimiento de tu hijo y su comportamiento en las clases.</p>
+  <section id="one" class="wrapper">
+    <div class="inner">
+      <div class="flex flex-3">
+        <article>
+          <header>
+            <h3>Comunicación<br /> </h3>
+          </header>
+          <p>Educazone permite la comunicación entre cualquier institución dedicada a la educación y
+los padres de los alumnos que se encuentran en la misma mediante distintas
+funcionalidades como la agenda personal del alumno, mensajería instantánea, la ficha
+personal de cada alumno y el aula virtual donde se compartirán distintos contenidos de las
+asignaturas.</p>
+          <footer>
+            <a href="signin.php" class="button special">More</a>
+          </footer>
+        </article>
+        <article>
+          <header>
+            <h3> Donde quieras y cuando quieras</h3>
+          </header>
+          <p>Los usuarios registrados podrán recibir notificaciones de notas, menú del comedor ,
+circulares, tutorías, actividades extraescolares, excursiones de sus hijos
+matriculados en un centro educativo. Además existe la posibilidad de comunicación
+instantánea, sencilla y efectiva entre las familias y los profesores del centro
+educativo.</p>
+          <footer>
+            <a href="signin.php" class="button special">More</a>
+          </footer>
+        </article>
+        <article>
+          <header>
+            <h3>Profesionalidad</h3>
+          </header>
+          <p>Profesionales del sector tendrán acceso a mensajería con los familiares del
+alumno matriculado en su clase y podrán informar de toda actividad diaria e informar
+sobre el progreso y desarrollo del alumno.</p>
+          <footer>
+            <a href="signin.php" class="button special">More</a>
+          </footer>
+        </article>
       </div>
     </div>
+  </section>
 
-    <div class="cuadrados">
-      <div class="cuadrado">
-        <h1>Progreso a tiempo real</h1>
-        <p>Puedes visualizar las calificaciones de tus hijos sin tener que pasar por el colegio.</p>
-      </div>
-
-      <div class="cuadrado">
-        <h1>Sencillo, intuitivo y útil</h1>
-        <p>Con una interfaz gráfica sencilla y clara mantenemos una máxima funcionalidad para que no te tengas que preocupar de nada.</p>
+  <section id="two" class="wrapper style1 special">
+    <div class="inner">
+      <header>
+        <h2>Equipo</h2>
+        <p>¿Quiénes somos? Ponte en contacto con nosotros</p>
+      </header>
+      <div class="flex flex-4">
+        <div class="box person">
+          <div class="image round">
+            <img src="img/users/empleados/empleado1.jpg" alt="Person 1" />
+          </div>
+          <h3>Carlos Pérez</h3>
+          <p>CEO</p>
+          <p>CarCer@gmail.com</p>
+        </div>
+        <div class="box person">
+          <div class="image round">
+            <img src="img/users/empleados/empleado2.jpg" alt="Person 2" />
+          </div>
+          <h3>Laura García</h3>
+          <p>Programador</p>
+          <p>LauGar@gmail.com</p>
+        </div>
+        <div class="box person">
+          <div class="image round">
+            <img src="img/users/empleados/empleado4.jpg" alt="Person 3" />
+          </div>
+          <h3>Pablo Oliveras</h3>
+          <p>Director de ventas</p>
+          <p>PablOl@gmail.com</p>
+        </div>
+        <div class="box person">
+          <div class="image round">
+            <img src="img/users/empleados/empleado3.jpg" alt="Person 4" />
+          </div>
+          <h3>Dolores Pérez</h3>
+          <p>Programador</p>
+          <p>DoloresP@gmail.com</p>
+        </div>
       </div>
     </div>
+  </section>
+
     <?php
        include("include/comun/pie.php");
     ?>
