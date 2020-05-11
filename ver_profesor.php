f8fade12b2c63141cf697c212f3eb454b5a6c762
Rymond3 <ramoncos@ucm.es>
2020-05-11 02:21:04 +0200

    Update ver_profesor.php

--- ver_profesor.php
+++ ver_profesor.php
@@ -7,7 +7,7 @@ require_once __DIR__ . '/include/config.php';
   <head>
     <meta charset="utf-8">
     <title>Profesor</title>
-    <link rel="stylesheet" type="text/css" href="css/estilo.css">
+    <link rel="stylesheet" type="text/css" href="css/VerPadreyProfe.css">
   </head>
   <body>
    <div id ="profesor">
@@ -21,6 +21,7 @@ require_once __DIR__ . '/include/config.php';
       }
     ?>
     <div id="contenido">
+       <div class="cuadradoInfo">
       <?php
         $profesor = new Profesor();
 
@@ -50,30 +51,54 @@ require_once __DIR__ . '/include/config.php';
           $msg = NULL;
           echo "<p><a href=\"mensajeria.php?tutor=".$id_padre."&profesor=".$profesor->getId()."&contenido_msg=".$msg."\">Enviar mensaje</a></p>";
         }
-          if ($_SESSION['rol'] == "profesor"){
-      ?>
+         ?>
+    </div>
+            <div class="cuadrados">
+        <div class="cuadrado">
+        <h1><a href = "Editar_padre.php">Editar Perfil</a></h1>
+        <p>Modifica tu perfil para conocer todos tus datos.</p>
+        </div>
 
+       <div class="cuadrado">
+        <h1><a href = "foro_seleccion.php">Foro</a></h1>
+        <p>Comparte historias, fotos y buenos recuerdos de las actividades con el colegio.</p>
+       </div>
 
-
-    <div id="Editar">
-
-      <form method="post">
-        <input type="submit" name="Edit" value="EDITAR">
-      </form>
+      <div class="cuadrado">
+        <h1><a href = "mensajeriaClases.php">Mensajeria</a></h1>
+        <p>Intercambia mensajes y archivos con los profesores para mantenerte informado de todo lo necesario sobre las clases de tu hijo.</p>
+      </div>
 
     </div>
-    <?php
-  }?>
+
+      <div class="cuadrados2">
+        <div class="cuadrado">
+        <h1><a href = "horario_profesor.php">Horario</a></h1>
+        <p>Consulta el horario de tus clases de cada semana.</p>
+        </div>
+
+       <div class="cuadrado">
+        <h1><a href = "cursos.php">Cursos</a></h1>
+        <p>Accede a la información de los cursos, clases y alumnos del colegio.</p>
+       </div>
+
+      <div class="cuadrado">
+        <h1>Calendario</a></h1>
+        <p>Revisa tus eventos en el calendario mensual.</p>
+      </div>
+      
+    </div>
+  </div>
 </div>
     <?php
 
 
       if($_SESSION['rol'] == 'profesor'){
-         if(isset($_POST["Edit"])){
-              include("EditarProfe.php");
-            }
+        include("include/comun/sidebarDerProfesor.php");
       }
-
+      else{
+        include("include/comun/sidebarDerPadre.php");
+      }
       include("include/comun/pie.php");
 
 
