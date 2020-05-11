65609266d7d37c1a94e40119c1a1631471510921
Rymond3 <ramoncos@ucm.es>
2020-05-11 02:29:54 +0200

    Update MensajeriaAlumnos.php

--- MensajeriaAlumnos.php
+++ MensajeriaAlumnos.php
@@ -30,11 +30,20 @@ require_once __DIR__ . '/include/config.php';
         if($rs->num_rows > 0){
           $i = 1;
           while($fila = $rs->fetch_assoc()){
-            echo '<form name="myform" action="mensajeria.php" method="POST">
-              <input type="hidden" name="tutor" value="' .$fila["id_tutor_legal"]. '">
-              <input type="hidden" name="profesor" value="' .htmlspecialchars(trim(strip_tags($_GET["profesor"]))). '">
-              <button type="submit">' .$i. '. Tutor legal de ' .$fila["nombre"]. ' ' .$fila["apellido1"]. ' ' .$fila["apellido2"]. '</button>
-            </form>';
+            ?>
+            <div class="bloque">
+              <img src="img/users/alumnos/<?php echo $fila["nombre"]; ?>.jpg" style="width:100%">
+              <div class="container">
+                <?php
+                echo '<form name="myform" action="mensajeria.php" method="POST">
+                  <input type="hidden" name="tutor" value="' .$fila["id_tutor_legal"]. '">
+                  <input type="hidden" name="profesor" value="' .htmlspecialchars(trim(strip_tags($_GET["profesor"]))). '">
+                  <button type="submit">' .$i. '. Tutor legal de ' .$fila["nombre"]. ' ' .$fila["apellido1"]. ' ' .$fila["apellido2"]. '</button>
+                  </form>';
+                ?>
+              </div>
+            </div>
+            <?php
             $i = $i + 1;
           }
         }
@@ -45,6 +54,7 @@ require_once __DIR__ . '/include/config.php';
     </div>
 
     <?php
+      include("include/comun/sidebarDerProfesor.php");
       include("include/comun/pie.php");
     ?>
    </div>
